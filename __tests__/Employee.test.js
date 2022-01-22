72844d8ffc867878657e6c67b374100fd477860c
paul88vera <paul88vera@gmail.com>
2022-01-22 08:11:38 -0600

    test 2 employee info passed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require('../lib/Employee');
+
+// jest.mock('../lib/Employee');
+
+test('get employee stats as an object', () => {
+ const employee = new Employee('Jessica', 'RaBb8t', 'rabb8@gmail.com');
+
+ expect(employee.name).toBe('Jessica');
+ expect(employee.id).toBe('RaBb8t');
+ expect(employee.email).toBe('rabb8@gmail.com');
+});
